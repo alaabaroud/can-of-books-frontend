9de29e95bda1bd5dd000df3f338084c6ea730665
obada-naser <obadanaser135@gmail.com>
2021-08-11 02:58:45 +0300

    trying to solve lab 12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
-import Books from './Books'
+import Books from './Books';
 import './BestBooks.css';
 
 
@@ -65,3 +65,79 @@ class MyFavoriteBooks extends React.Component {
 }
 
 export default withAuth0(MyFavoriteBooks);
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Jumbotron from 'react-bootstrap/Jumbotron';
+// import { withAuth0 } from '@auth0/auth0-react';
+// import axios from 'axios';
+// import Books from './Books'
+// import './BestBooks.css';
+
+
+// class MyFavoriteBooks extends React.Component {
+//     constructor(props){
+//       super(props)
+//       this.state={
+//         book:[],
+//       }
+//     }
+
+//      renderBooks=async()=>{
+//       const{user}=this.props.auth0;
+//       console.log(user);
+//       try{
+//         let url=`http://localhost:3001/book?email=${user.email}`;
+        
+
+//         let data= await axios.get(url);
+//         this.setState({
+//           book:data.data
+//         })
+//       }
+//       catch(error){
+//         if(error.response && error.response.data)
+//         console.log('error');
+//       }
+//     } 
+
+//     componentDidMount=()=>{
+//       this.renderBooks();
+//     }
+
+
+
+//   render() {
+
+
+//     const {  isAuthenticated } = this.props.auth0;
+    
+//     console.log(this.props.auth0);
+//     return(
+//       <Jumbotron>
+
+// { isAuthenticated && 
+//            <> 
+//         <h1>My Favorite Books</h1>
+//         <p>
+          
+//           This is a collection of my favorite books
+//         </p>
+//         <Books arr={this.state.book}/>
+     
+
+//       </>}
+//       </Jumbotron>
+//     )
+//   }
+// }
+
+// export default withAuth0(MyFavoriteBooks);
