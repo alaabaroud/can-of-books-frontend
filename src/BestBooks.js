c9c8b18c4feda77305fd18366ff9cc5b0f8fe906
obada-naser <obadanaser135@gmail.com>
2021-08-14 03:39:58 +0300

    making the post

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -5,7 +5,8 @@ import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 import Books from './Books';
 import './BestBooks.css';
-import BookFormModal from './BookFormModal'
+import BookFormModal from './BookFormModal';
+import Button from 'react-bootstrap/Button';
 
 
 class MyFavoriteBooks extends React.Component {
@@ -13,16 +14,16 @@ class MyFavoriteBooks extends React.Component {
       super(props)
       this.state={
         book:[],
-        email : '',
+        email:'',
         // server: process.env.REACT_APP_SERVER_URL
-        // showBooksComponent:false
+         showBooks:false
 
       }
     }
 
      renderBooks=async()=>{
       const{user}=this.props.auth0;
-      console.log(user.email);
+      // console.log(user.email);
       try{
         let url=`http://localhost:3001/book?email=${user.email}`;
         
@@ -42,15 +43,19 @@ class MyFavoriteBooks extends React.Component {
     componentDidMount=()=>{
       this.renderBooks();
     }
-
+    handleModel=()=>{
+      this.setState({
+        showBooks:!this.state.showBooks
+      })
+    }
    
      addBook = async (event) => {
       event.preventDefault();
 
         const title = event.target.title.value;
          const description = event.target.description.value;
-         const status = event.target.status.value
-        //  const email = event.target.email.value
+         const status = event.target.status.value;
+        //  const email = event.target.email.value;
 
         
          
@@ -66,34 +71,34 @@ class MyFavoriteBooks extends React.Component {
       }
       // console.log(this.state.email);
 
-      const options = {
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: { 'Content-Type': 'application/json' },
-      };
-      await axios.post('http://localhost:3001/add', JSON.stringify(bookForm), options).catch(console.error);
-      console.log(bookForm);
+      // const options = {
+      //   mode: 'cors',
+      //   cache: 'no-cache',
+      //   headers: { 'Content-Type': 'application/json' },
+      // };
+      // await axios.post('http://localhost:3001/add', JSON.stringify(bookForm), options).catch(console.error);
+      // console.log(bookForm);
      
-    //   // let URL=`http://localhost:3001`;
-    //  await  axios({
-    //     method: 'post',
-    //     url: 'http://localhost:3001/add',
-    //     data:  bookForm
+      // let URL=`http://localhost:3001`;
+     await  axios({
+        method: 'post',
+        url: 'http://localhost:3001/add',
+        data:  bookForm
         
-    //   })
-    //   // await axios
-    //   // .post(`http://localhost:3001/addbooks`, bookForm)
-    //   .then( data => {
+      })
+      // await axios
+      // .post(`http://localhost:3001/addbooks`, bookForm)
+      .then( data => {
 
-    //     console.log(data);
-    //     this.setState({
-    //       book : data.data
-    //     })
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   })
-    //   console.log(event.target.title.value);
+        console.log(data);
+        this.setState({
+          book : data.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      console.log(event.target.title.value);
     }
     
 
@@ -110,17 +115,26 @@ class MyFavoriteBooks extends React.Component {
 
 { isAuthenticated && 
            <> 
+
         <h1>My Favorite Books</h1>
         <p>
           
           
           This is a collection of my favorite books
         </p>
+        
         <Books arr={this.state.book}/>
+        <Button onClick={this.handleModel}>
+          Add a book
+
+        </Button>
+        {this.state.showBooks &&
         <BookFormModal
+        show={this.state.showBooks}
+        handleClose={this.handleModel}
         addBook={this.addBook}
         />
-     
+        }
 
       </>}
       </Jumbotron>
